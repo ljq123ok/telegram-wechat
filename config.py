58df1d846acbcc91b6c9685a18db570229beeded
ljq123ok <62369793+ljq123ok@users.noreply.github.com>
2023-02-20 13:53:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ master_channel = 'plugins.eh_telegram_master', 'TelegramChannel'
 slave_channels = [('plugins.eh_wechat_slave', 'WeChatChannel')]
 
 eh_telegram_master = {
-    "token": "xxxxxxxxxxxxxxxxx", # YOUR_TELEGRAM_BOT_TOKEN
-    "admins": [1234567890],       # YOUR_TELEGRAM_ID
+    "token": "6094168203:AAGmaLW1YWOBGb8ZiNg4cj1bF_5kR40hILU", # YOUR_TELEGRAM_BOT_TOKEN
+    "admins": [1271109936],       # YOUR_TELEGRAM_ID
     "bing_speech_api": ["xxx", "xxx"],
     "baidu_speech_api": {
         "app_id": 0,
